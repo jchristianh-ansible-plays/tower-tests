ce412f147975c757933992235b9876232e9b7322
Chris Hammer <chris.hammer@redhat.com>
2022-03-31 12:40:51 -0400

    add saml auth notes for lab, and a secondary test script

---
+++ utils/dev_tower_test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+CONC="50"
+REQU="100"
+
+BASE_URL="10.1.1.51"
+AAP_JOB_ID="10"
+POST_PAYLOAD_PATH="empty.post"
+#POST_PAYLOAD_PATH="provision.post"
+POST_PAYLOAD_TYPE="application/json"
+
+TOWER_USER="admin"
+TOWER_PASS="redhat"
+
+
+printf "\n%s\n" \
+  "# Running test - ${REQU} requests / ${CONC} concurrency"
+  ab -c "${CONC}" \
+  -n ${REQU} \
+  -A ${TOWER_USER}:${TOWER_PASS} \
+  -T "${POST_PAYLOAD_TYPE}" \
+  -v1 \
+  -p ${POST_PAYLOAD_PATH} "https://${BASE_URL}/api/v2/job_templates/${AAP_JOB_ID}/launch/" | \
+  grep "Failed requests\|Time taken for tests"
